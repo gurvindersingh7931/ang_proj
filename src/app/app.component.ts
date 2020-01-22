835221a9327e0286fd456eb4af2483c465bf5b57
Gurvinder Singh <gurvindersingh7931@gmail.com>
2020-01-23 00:07:53 +0530

    Section 6 3 videos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'proj';
+  loadFeature = "recipe";
+  onNavigate(feature: string){
+    this.loadFeature = feature;
+  }
 }
